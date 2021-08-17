8ed0e29a1d59e27e99042068da38a6bc55f984fb
gooodbyyy <85493958+gooodbyyy@users.noreply.github.com>
2021-08-17 17:15:56 +0300

    add: solve a task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+//min value
+exports.min = function min(array) {
+        if (!Array.isArray(array) || array.length === 0) {
+            return 0;
+        }
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+        return min;
+    }
+    //max value
+exports.max = function max(array) {
+        if (!Array.isArray(array) || array.length === 0) {
+            return 0;
+        }
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
+        }
+        return max;
+    }
+    //average value
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
 }
